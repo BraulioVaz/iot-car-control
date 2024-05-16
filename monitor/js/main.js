32f8d4661f0aae0f8ea1b7d50e5e390e3dc76821
Braulio <brauliovaz1220@gmail.com>
2024-05-16 11:26:55 -0600

    Second commit

---
+++ monitor/js/main.js
@@ -0,0 +1,38 @@
+var mensaje = document.getElementById("respuesta");
+
+function callApi(){
+	axios.get('http://52.14.56.206/backend/apis/getRegistro.php')
+	  .then(function (response) {
+	    console.log(response);
+	    
+	    let text = "";
+	    switch(response.data){
+	    	case 'S':
+	    		text = "STOP";
+	    	break;
+	    	case 'F':
+	    		text = "FORWARD";
+	    	break;
+	    	case 'B':
+	    		text = "BACKWARD";
+	    	break;
+	    	case 'L':
+	    		text = "LEFT";
+	    	break;
+	    	case "R":
+	    		text = "RIGHT";
+	    	break;
+	    }
+	    
+	    mensaje.innerHTML = "Respuesta : " + text;
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  })
+	  .finally(function () {
+	});
+}
+
+window.onload = function(){
+	setInterval(callApi, 2000);
+}
